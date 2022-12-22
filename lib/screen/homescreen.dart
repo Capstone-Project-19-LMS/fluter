0d4416efc1bdbd2703483c6d9ea9463d3b13a48f
Hernan Febri <hernanfebri@gmail.com>
2022-12-22 15:10:35 +0700

    Update Api and Get in My Course Screen

--- lib/screen/homescreen.dart
+++ lib/screen/homescreen.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:google_fonts/google_fonts.dart';
